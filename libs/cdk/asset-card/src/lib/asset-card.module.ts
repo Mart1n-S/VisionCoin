2d7ec2dbc5dcbe753594ba403b2730b3cf7d6536
Mart1n-S <martinsimongo@gmail.com>
2022-06-01 12:39:59 +0200

    feature-angular-component-1

--- libs/cdk/asset-card/src/lib/asset-card.module.ts
+++ libs/cdk/asset-card/src/lib/asset-card.module.ts
@@ -1,9 +1,11 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
 import { AssetCardComponent } from './asset-card.component';
 
 @NgModule({
-  imports: [CommonModule],
+  imports: [CommonModule,
+    MatCardModule],
   declarations: [
     AssetCardComponent
   ],
